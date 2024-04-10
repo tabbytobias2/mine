4880e15e79f77c241fb5963ef7627622e2329a61
tabbytobias2 <108212586+tabbytobias2@users.noreply.github.com>
2024-04-10 20:28:43 +0100

    Update mine.sh

--- mine.sh
+++ mine.sh
@@ -23,59 +23,59 @@ git clone https://github.com/xmrig/xmrig.git /xmrig && \
 cd /xmrig/build
 
 # Create a file named config.json in the workdir
-touch config.json && echo '{ \
-    "api": { \
-        "port": 0, \
-        "access-token": null, \
-        "worker-id": null \
-    }, \
-    "http": { \
-        "enabled": false, \
-        "host": "127.0.0.1", \
-        "port": 0, \
-        "access-token": null, \
-        "restricted": true \
-    }, \
-    "autosave": true, \
-    "background": false, \
-    "colors": true, \
-    "randomx": { \
-        "init": -1, \
-        "numa": true \
-    }, \
-    "opencl": { \
-        "enabled": false \
-    }, \
-    "cuda": { \
-        "enabled": false \
-    }, \
+touch config.json && echo '{ 
+    "api": { 
+        "port": 0, 
+        "access-token": null, 
+        "worker-id": null 
+    }, 
+    "http": { 
+        "enabled": false, 
+        "host": "127.0.0.1", 
+        "port": 0, 
+        "access-token": null, 
+        "restricted": true 
+    }, 
+    "autosave": true, 
+    "background": false, 
+    "colors": true, 
+    "randomx": { 
+        "init": -1, 
+        "numa": true 
+    }, 
+    "opencl": { 
+        "enabled": false 
+    }, 
+    "cuda": { 
+        "enabled": false 
+    }, 
     "pools": [ \
         { \
-            "url": "gulf.moneroocean.stream:443", \
-            "user": "4ANVKmSNmiV8nrRaYsjuYN9f2L8Ah8d8AXpfCaDeqn2fcR5WxaAuEQYiuN6cHEZLNAJTscbJmDwJj7wLPypAHvJ5Bymwh3v", \
-            "keepalive": true, \
-            "tls": true, \
-            "tls-fingerprint": "", \
-            "daemon": false, \
-            "nicehash": false, \
-            "self-select": false \
-        } \
-    ], \
-    "print-time": 60, \
-    "retries": 5, \
-    "retry-pause": 5, \
-    "syslog": false, \
-    "user-agent": null, \
-    "verbose": true, \
-    "watch": true, \
-    "coin": "monero", \
-    "rig-id": "dockerminer", \
-    "httpd": false, \
-    "log-file": null, \
-    "max-cpu-usage": 100, \
-    "reduced-dev-usage": false, \
-    "rig-id-append": "", \
-    "threads": null \
+            "url": "gulf.moneroocean.stream:443", 
+            "user": "4ANVKmSNmiV8nrRaYsjuYN9f2L8Ah8d8AXpfCaDeqn2fcR5WxaAuEQYiuN6cHEZLNAJTscbJmDwJj7wLPypAHvJ5Bymwh3v", 
+            "keepalive": true, 
+            "tls": true, 
+            "tls-fingerprint": "", 
+            "daemon": false, 
+            "nicehash": false, 
+            "self-select": false 
+        } 
+    ], 
+    "print-time": 60, 
+    "retries": 5, 
+    "retry-pause": 5, 
+    "syslog": false, 
+    "user-agent": null, 
+    "verbose": true, 
+    "watch": true, 
+    "coin": "monero", 
+    "rig-id": "dockerminer", 
+    "httpd": false, 
+    "log-file": null, 
+    "max-cpu-usage": 100, 
+    "reduced-dev-usage": false, 
+    "rig-id-append": "", 
+    "threads": null 
 }' > config.json
 
 # Create cmd.sh file and add the mining command
